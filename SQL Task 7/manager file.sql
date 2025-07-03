b26b8c1da8706dd9e22a6f5b6489be5f03c3e73a
shomesh-28 <shomeshgupta321@gmail.com>
2025-07-03 21:24:19 +0530

    Update manager file.sql

--- SQL Task 7/manager file.sql
+++ SQL Task 7/manager file.sql
@@ -1,1 +1,4 @@
+-- in order to run this query we need to connect the manager username to our database. In my file, the name of the database was postgre and i connnected the username 'manager' to postgre database. 
+-- In order to maintain data integrity i only provided the manager with the select statement.
+
 select * from sales_summary;
