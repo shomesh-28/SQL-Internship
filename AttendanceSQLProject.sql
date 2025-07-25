a45a68bb0bd3c29966f5e86db6c9620993281c4e
Shomesh Shashidhar Gupta <shomeshgupta321@gmail.com>
2025-07-25 17:45:04 +0530

    Add files via upload

--- AttendanceSQLProject.sql
+++ AttendanceSQLProject.sql
@@ -8,9 +8,12 @@ constraint depFK foreign key(department_id) references department(dep_id),constr
 create table attendance(attendance_id int,emp_id int,attendance_date date,check_in time,
 constraint empIdFK foreign key(emp_id) references employee(id),constraint attPK primary key(emp_id,attendance_date));
 drop table department,employee,attendance;
+delete from department
+insert into department values(1,'IT'),(2,'HR'),(3,'Manager');
+insert into employee values(18,'John','Doe','000-000-0000','john@example.com','Mumbai','2021-12-8',1,1),
+(19,'Novak','Djokovic','111-111-1111','novak@example.com','Delhi','2022-1-31',2,1),
+(1,'Sundar','Pichai','222-222-2222','sundar@example.com','Pune','2020-12-1',3,null);
 
-insert into department values(1,'Hr');
-insert into employee values(1,'sh','gu','000-000-0000','sjdfak','Mum',current_date,1,1);
 create or replace function attFun() returns Trigger as $$
 begin 
 update attendance set check_in = current_time where attendance_id = attendance_id;
